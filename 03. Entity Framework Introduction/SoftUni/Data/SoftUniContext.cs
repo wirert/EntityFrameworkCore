88aca07e45056a2676d55afd59e2fc0422122b1c
wirert <wirert@students.softuni.bg>
2023-02-25 10:09:37 +0200

    rename dir and add static method in StartUp

--- 03. Entity Framework Introduction/SoftUni/Data/SoftUniContext.cs
+++ 03. Entity Framework Introduction/SoftUni/Data/SoftUniContext.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+﻿using Microsoft.EntityFrameworkCore;
 using SoftUni.Models;
 
 namespace SoftUni.Data
 {
-    public partial class SoftUniContext : DbContext
+    public class SoftUniContext : DbContext
     {
         public SoftUniContext()
         {
@@ -22,7 +19,7 @@ namespace SoftUni.Data
         public virtual DbSet<Employee> Employees { get; set; } = null!;
         public virtual DbSet<Project> Projects { get; set; } = null!;
         public virtual DbSet<Town> Towns { get; set; } = null!;
-        public virtual DbSet<EmployeesProjects> EmployeesProjects { get; set; } = null!;
+        public virtual DbSet<EmployeeProject> EmployeesProjects { get; set; } = null!;
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -36,6 +33,14 @@ namespace SoftUni.Data
         {
             modelBuilder.Entity<Address>(entity =>
             {
+                entity.Property(e => e.AddressId).HasColumnName("AddressID");
+
+                entity.Property(e => e.AddressText)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.TownId).HasColumnName("TownID");
+
                 entity.HasOne(d => d.Town)
                     .WithMany(p => p.Addresses)
                     .HasForeignKey(d => d.TownId)
@@ -44,6 +49,14 @@ namespace SoftUni.Data
 
             modelBuilder.Entity<Department>(entity =>
             {
+                entity.Property(e => e.DepartmentId).HasColumnName("DepartmentID");
+
+                entity.Property(e => e.ManagerId).HasColumnName("ManagerID");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
                 entity.HasOne(d => d.Manager)
                     .WithMany(p => p.Departments)
                     .HasForeignKey(d => d.ManagerId)
@@ -53,6 +66,34 @@ namespace SoftUni.Data
 
             modelBuilder.Entity<Employee>(entity =>
             {
+                entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+
+                entity.Property(e => e.AddressId).HasColumnName("AddressID");
+
+                entity.Property(e => e.DepartmentId).HasColumnName("DepartmentID");
+
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.HireDate).HasColumnType("smalldatetime");
+
+                entity.Property(e => e.JobTitle)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ManagerId).HasColumnName("ManagerID");
+
+                entity.Property(e => e.MiddleName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Salary).HasColumnType("money");
+
                 entity.HasOne(d => d.Address)
                     .WithMany(p => p.Employees)
                     .HasForeignKey(d => d.AddressId)
@@ -67,12 +108,46 @@ namespace SoftUni.Data
                 entity.HasOne(d => d.Manager)
                     .WithMany(p => p.InverseManager)
                     .HasForeignKey(d => d.ManagerId)
-                    .HasConstraintName("FK_Employees_Employees");                
+                    .HasConstraintName("FK_Employees_Employees");
+
+                
             });
 
-            modelBuilder.Entity<EmployeesProjects>(entity =>
+            modelBuilder.Entity<Project>(entity =>
             {
-                entity.HasKey(ep => new { ep.EmployeeId, ep.ProjectId });
+                entity.Property(e => e.ProjectId).HasColumnName("ProjectID");
+
+                entity.Property(e => e.Description).HasColumnType("ntext");
+
+                entity.Property(e => e.EndDate).HasColumnType("smalldatetime");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.StartDate).HasColumnType("smalldatetime");
+            });
+
+            modelBuilder.Entity<Town>(entity =>
+            {
+                entity.Property(e => e.TownId).HasColumnName("TownID");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<EmployeeProject>(entity =>
+            {
+                entity.HasKey(pk => new { pk.EmployeeId, pk.ProjectId });
+
+                entity.HasOne(ep => ep.Employee)
+                    .WithMany(e => e.EmployeesProjects)
+                    .HasForeignKey(ep => ep.EmployeeId);
+
+                entity.HasOne(ep => ep.Project)
+                    .WithMany(p =>p.EmployeesProjects)
+                    .HasForeignKey(ep =>ep.ProjectId);                    
             });
         }
     }
---
+++ 03. Entity Framework Introduction/SoftUni/StartUp.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using System.Text;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext context = new SoftUniContext();
+
+        Console.WriteLine(GetEmployeesFullInformation(context));
+    }
+
+    public static string GetEmployeesFullInformation(SoftUniContext context)
+    {
+        StringBuilder sb= new StringBuilder();
+
+        var employees = context.Employees
+            .AsNoTracking()
+            .OrderBy(e => e.EmployeeId)
+            .Select(e => new
+            {
+                e.FirstName,
+                e.LastName,
+                e.MiddleName,
+                e.JobTitle,
+                e.Salary
+            });            
+
+        foreach (var e in employees)
+        {
+            sb.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}");
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+}
