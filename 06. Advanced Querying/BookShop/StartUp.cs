d310426f378597ec53580a6699b3515c026b08be
Krasimir Nikolov <wirert@gmail.com>
2023-03-03 19:25:38 +0200

    added some methods in StartUp

--- 06. Advanced Querying/BookShop/StartUp.cs
+++ 06. Advanced Querying/BookShop/StartUp.cs
@@ -13,7 +13,7 @@
             using var db = new BookShopContext();
             DbInitializer.ResetDatabase(db);
 
-            Console.WriteLine(GetBooksByAgeRestriction(db, "teEN"));
+            Console.WriteLine(GetBooksNotReleasedIn(db, 2000));
         }
 
         public static string GetBooksByAgeRestriction(BookShopContext context, string command)
@@ -31,6 +31,43 @@
 
             return sb.ToString().TrimEnd();
         }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            var books = context.Books
+                .AsNoTracking()
+                .Where(b => b.EditionType == EditionType.Gold && b.Copies < 5000)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, books);
+        }
+
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            var books = context.Books
+                .AsNoTracking()
+                .Where(b => b.Price > 40)
+                .OrderByDescending(b => b.Price)
+                .Select(b => $"{b.Title} - ${b.Price:f2}")
+                .ToArray();
+
+            return string.Join(Environment.NewLine, books);
+        }
+
+        public static string GetBooksNotReleasedIn(BookShopContext context, int year)
+        {
+            var books = context.Books
+                .AsNoTracking()
+                .Where(b => b.ReleaseDate!.Value.Year != year)
+                .OrderBy(b => b.BookId)
+                .Select (b => b.Title)
+                .ToArray();
+
+            return string.Join (Environment.NewLine, books);
+        }
+
     }
 }
 
