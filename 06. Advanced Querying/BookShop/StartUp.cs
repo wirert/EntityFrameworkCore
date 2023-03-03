268b4b3c0d0b320689d66dd9f257ffe06471e747
Krasimir Nikolov <wirert@gmail.com>
2023-03-03 20:34:04 +0200

    added methods

--- 06. Advanced Querying/BookShop/StartUp.cs
+++ 06. Advanced Querying/BookShop/StartUp.cs
@@ -4,7 +4,9 @@
     using Data;
     using Initializer;
     using Microsoft.EntityFrameworkCore;
+    using System.Linq;
     using System.Text;
+    using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
     public class StartUp
     {
@@ -13,9 +15,10 @@
             using var db = new BookShopContext();
             DbInitializer.ResetDatabase(db);
 
-            Console.WriteLine(GetBooksNotReleasedIn(db, 2000));
+            Console.WriteLine(GetBooksReleasedBefore(db, "12-04-1992"));
         }
 
+        // 02. Age Restriction
         public static string GetBooksByAgeRestriction(BookShopContext context, string command)
         {
             StringBuilder sb = new StringBuilder();
@@ -32,6 +35,7 @@
             return sb.ToString().TrimEnd();
         }
 
+        //03. Golden Books
         public static string GetGoldenBooks(BookShopContext context)
         {
             var books = context.Books
@@ -44,6 +48,7 @@
             return string.Join(Environment.NewLine, books);
         }
 
+        //04. Books by Price
         public static string GetBooksByPrice(BookShopContext context)
         {
             var books = context.Books
@@ -56,6 +61,7 @@
             return string.Join(Environment.NewLine, books);
         }
 
+        //05. Not Released In
         public static string GetBooksNotReleasedIn(BookShopContext context, int year)
         {
             var books = context.Books
@@ -66,6 +72,38 @@
                 .ToArray();
 
             return string.Join (Environment.NewLine, books);
+        }
+
+        //06. Book Titles by Category
+        public static string GetBooksByCategory(BookShopContext context, string input)
+        {
+            var inputCategories = input.ToLower().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            var books = context.Books
+                .AsNoTracking()
+                .Include(b => b.BookCategories)
+                .ThenInclude(bc => bc.Category)
+                .Where(b => b.BookCategories.All(c => inputCategories.Contains(c.Category.Name.ToLower())))
+                .Select(b => b.Title)
+                .OrderBy(b => b)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, books);
+        }
+
+        //07. Released Before Date
+        public static string GetBooksReleasedBefore(BookShopContext context, string date)
+        {
+            var dateAsDateTime = DateTime.ParseExact(date, "dd-MM-yyyy", System.Globalization.CultureInfo.InvariantCulture);
+
+            var books = context.Books
+                .AsNoTracking()
+                .Where(b => b.ReleaseDate.Value.Date.CompareTo(dateAsDateTime) < 0)
+                .OrderByDescending(b => b.ReleaseDate)
+                .Select(b => $"{b.Title} - {b.EditionType} - ${b.Price:f2}")
+                .ToArray();
+
+            return string.Join(Environment.NewLine, books);
         }
 
     }
