8d7201b1ebc33d331e04e2ec0aabec4be33324fe
Krasimir Nikolov <wirert@gmail.com>
2023-03-03 18:26:36 +0200

    02. AgeRestriction method

---
+++ 06. Advanced Querying/BookShop/StartUp.cs
@@ -0,0 +1,37 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(GetBooksByAgeRestriction(db, "teEN"));
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var books = context.Books
+                            .AsNoTracking()
+                            .Where(b => b.AgeRestriction == (AgeRestriction)Enum.Parse(typeof(AgeRestriction), command, true))
+                            .Select(b => b.Title)
+                            .OrderBy(b => b)
+                            .ToArray();
+
+            sb.Append(string.Join(Environment.NewLine, books));
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
+
+
