9124557ff2d7d980595021f8e32cb09813b513ab
Krasimir Nikolov <wirert@gmail.com>
2023-03-11 09:00:33 +0200

    not finished

---
+++ 07. C# Auto Mapping Objects/FastFood.Core/Controllers/CategoriesController.cs
@@ -0,0 +1,54 @@
+﻿namespace FastFood.Web.Controllers
+{
+    using System;
+    using System.Linq;
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using Data;
+    using FastFood.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using ViewModels.Categories;
+
+    public class CategoriesController : Controller
+    {
+        private readonly FastFoodContext context;
+        private readonly IMapper mapper;
+
+        public CategoriesController(FastFoodContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateCategoryInputModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            var category = mapper.Map<Category>(model);
+
+            context.Categories.Add(category);
+
+            context.SaveChanges();
+
+            return RedirectToAction("All", "Categories");
+        }
+
+        public IActionResult All()
+        {
+            var categories = context.Categories
+                .ProjectTo<CategoryAllViewModel>(mapper.ConfigurationProvider)
+                .ToList();
+
+            return View(categories);
+        }
+    }
+}
